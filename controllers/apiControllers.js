2d7a30f152ce4eed89f32171a58ea6c2d2baefb7
sharankasandula <sharankasandula@gmail.com>
2019-01-17 14:54:50 +0100

    implemented new api's -get single customer -delete single customer -update single customer -post single new customer

--- controllers/apiControllers.js
+++ controllers/apiControllers.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var cust = require('../model/custModel');
+var bParser = require('body-parser');
 
 
 
@@ -10,29 +11,68 @@ var apiController = function (app) {
     var customers=[];
     app.get('/api/home', function (req, res) {
         cust.find({}, function (err, custs) {
-            if (err) {
-                throw err;
-            }
-            customers = custs.map((item) => [{
-                "id":item._id, 
-                "data":item.primaryData
-            }]);
+            if (err) throw err;
+
+            if (custs.length !== 0) {
+                customers = custs.map((item) => [{
+                    "id": item._id,
+                    "data": item.primaryData
+                }]);
+            } else customers = null;
+            
             res.send(customers)
         });
     });
-
+    
     // get single customer
     app.get('/api/customer/:id', function (req, res) {
-        console.log('------------------------------------');
-        console.log(req.params);
-        console.log('------------------------------------');
-        cust.findOne({_id:req.params.id}, function (err, customer) {
+        cust.findOne({_id:req.params.id}, function (err, result) {
             if (err) {
                 throw err;
+            }
+            if (!result) {
+                result =null;
+            }
+            res.send(result);
+        });
+    });
+    
+    // delete single customer
+    app.delete('/api/customer/:id', function (req, res) {
+        var query = { "_id.$oid": req.params.body };
+        cust.deleteOne(query, function (err, customer) {
+            if (err) {
+                throw err;
+            }
+            if (customer.ok===1) {
+                customer = true
             }
             res.send(customer);
         });
     });
+
+
+    app.use(bParser.json());
+    // update single customer
+    app.put('/api/customer/:id', function (req, res) {
+        cust.updateOne({ _id: req.params.id }, { $set: req.body}, function (err, customer) {
+            if (err) {
+                throw err;
+            }
+            res.send(true);
+        });
+    });
+
+    // post new customer
+    app.post('/api/customer', function (req, res) {
+        cust.create(req.body, function (err) {
+            if (err) {
+                throw err;
+            }
+            res.send(true);
+        });
+    });
+    
 
     
 
